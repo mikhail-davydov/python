8a42b0cb2ae48a4b3e29bd6e6bbb0f436854e139
Mikhail Davydov <mkhldvdv@yandex.ru>
2025-10-01 00:21:22 +0300

    add flask sandbox

---
+++ flask/README.md
@@ -0,0 +1,7 @@
+# Flask Sandbox
+
+## Description
+This is a Flask web application built using the QuickStart Tutorial
+
+## Links
+https://flask.palletsprojects.com/en/stable/quickstart/
---
+++ flask/app.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+
+@app.route('/hello')
+def hello():
+    name = request.args.get("name", "noname")
+    return f"Hello, {escape(name)}!"
+
+
+@app.get('/login')
+def login_get():
+    return render_template('login.html')
+
+
+@app.post('/login')
+def login_post():
+    name = request.form['username'] if request.form['username'] else 'noname'
+    return f"Hello, {escape(name)}!"
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
+
+
+@app.route('/json')
+def show_json():
+    name = request.args.get("name", "noname")
+    order_id = request.args.get("order_id", "None")
+    # apply auto jsonify to dict response
+    return {
+        'name': escape(name),
+        'orderId': escape(order_id),
+        'path': request.path,
+    }
+    # return jsonify({
+    #     'name': escape(name),
+    #     'orderId': escape(order_id),
+    #     'path': request.path,
+    # })
+
+
+if __name__ == '__main__':
+    app.run()
